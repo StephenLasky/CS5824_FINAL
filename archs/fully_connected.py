1210c26ff2740c97453b47a204a90f2b02b3ceab
StephenLasky <stephenjlasky@gmail.com>
2019-04-26 15:32:31 -0400

    Added a 4 layer model.

--- archs/fully_connected.py
+++ archs/fully_connected.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 
 class FC_1(nn.Module):
     def __init__(self, D_in, D_h, D_out):
-
         super(FC_1, self).__init__()
         self.relu = nn.ReLU()
         self.linear1 = nn.Linear(D_in,D_h)
@@ -16,7 +15,6 @@ class FC_1(nn.Module):
 
 class FC_2(nn.Module):
     def __init__(self, D_in, D_h, D_out):
-
         super(FC_2, self).__init__()
         self.relu = nn.ReLU()
         self.linear1 = nn.Linear(D_in,D_h)
@@ -27,4 +25,22 @@ class FC_2(nn.Module):
         h1 = self.relu(self.linear1(x))
         h2 = self.relu(self.linear2(h1))
         y_pred = self.relu(self.linear3(h2))
+        return y_pred
+
+class FC_4(nn.Module):
+    def __init__(self, D_in, D_h, D_out):
+        super(FC_4, self).__init__()
+        self.relu = nn.ReLU()
+        self.linear1 = nn.Linear(D_in,D_h)
+        self.linear2 = nn.Linear(D_h, D_h)
+        self.linear3 = nn.Linear(D_h, D_h)
+        self.linear4 = nn.Linear(D_h, D_h)
+        self.linear5 = nn.Linear(D_h, D_out)
+
+    def forward(self, x):
+        h1 = self.relu(self.linear1(x))
+        h2 = self.relu(self.linear2(h1))
+        h3 = self.relu(self.linear3(h2))
+        h4 = self.relu(self.linear4(h3))
+        y_pred = self.relu(self.linear5(h4))
         return y_pred
