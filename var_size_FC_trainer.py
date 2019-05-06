5da157243879baf988fe77badaf7617db374b751
StephenLasky <stephenjlasky@gmail.com>
2019-05-05 22:51:40 -0400

    Allows for file-naming argument

--- var_size_FC_trainer.py
+++ var_size_FC_trainer.py
@@ -15,26 +15,27 @@ def train_fc(learning_rate = 1e-4,
              display_info = True,
              hidden_width = 512,
              num_x_rows = 3,
-             num_y_rows = 1
+             num_y_rows = 1,
+             x_train_filename = "data/custom/x_train.npy",
+             y_train_filename =  "data/custom/y_train.npy",
+             shuffle_data = True
              ):
     TRAIN_SET_SIZE = batch_size * num_batches
     ROW_WIDTH = 32
     COL_HEIGHT = 32
     COLOR_CHANNELS = 3
 
-    X_TRAIN_FILENAME = "data/custom/x_train.npy"
-    Y_TRAIN_FILENAME = "data/custom/y_train.npy"
     try:
         print("Attempting to load x, y data from disk.")
-        x = np.load(X_TRAIN_FILENAME)
-        y = np.load(Y_TRAIN_FILENAME)
+        x = np.load(x_train_filename)
+        y = np.load(y_train_filename)
     except:
         # create the dataset that we will be training on
-        print("x,y data disk load failture. Generating from scratch...")
+        print("x,y data disk load failure. Generating from scratch...")
         ims = f.open_data_ims(50000)
         x, y = f.generate_custom_data(num_x_rows, num_y_rows, ims[0:TRAIN_SET_SIZE], 32, 32)
-        np.save(X_TRAIN_FILENAME, x)
-        np.save(Y_TRAIN_FILENAME, y)
+        np.save(x_train_filename, x)
+        np.save(y_train_filename, y)
     assert x.shape[0] == y.shape[0]     # ensure x,y are of equal lengths
 
     # recombined = f.combine_xy_ims(x,y)
@@ -51,7 +52,7 @@ def train_fc(learning_rate = 1e-4,
     print("Using device:{}, cuda:{}, pytorch:{}".format(device, torch.version.cuda, torch.__version__))
 
     # set up model, optimizer, scheduler
-    m = fc.FC_1(input_width, hidden_width, output_width)
+    m = fc.FC_2(input_width, hidden_width, output_width)
     if torch.cuda.is_available(): m = m.cuda()  # transfer model to cuda as needed
     lossFunction = nn.L1Loss()
     optimizer = torch.optim.Adam(m.parameters(), lr=learning_rate)
@@ -62,10 +63,12 @@ def train_fc(learning_rate = 1e-4,
     y = torch.tensor(y, dtype=torch.float)
     x, y = x / 256.0, y / 256.0  # regularize
 
+    print("Data set size:{}".format(x.shape[0]))
+
     train_dataset = datasets.VectorizedDataset(x, y)
     train_loader = datasets.DataLoader(dataset=train_dataset,
                                        batch_size=batch_size,
-                                       shuffle=True,
+                                       shuffle=shuffle_data,
                                        num_workers=0)
 
     # remember loss graph
@@ -75,7 +78,7 @@ def train_fc(learning_rate = 1e-4,
     train_start_time = time.time()
 
     for epoch in range(num_epochs):
-        if epoch != 0 and epoch % int(num_epochs / 10) == 0:
+        if num_epochs >= 10 and epoch != 0 and epoch % int(num_epochs / 10) == 0:
             print("{}% done! loss:{}".format(epoch / num_epochs * 100.0, losses[epoch - 1]))
 
         epoch_losses = 0.0
@@ -97,7 +100,7 @@ def train_fc(learning_rate = 1e-4,
     print("Training took {} seconds!".format(time.time() - train_start_time))
 
     # our model is now trained. now let us attempt to reconstruct samples from our train set
-    VAL_SET_SIZE = 10
+    VAL_SET_SIZE = 100
     ims = f.open_data_ims(10000)[0:VAL_SET_SIZE]
 
     # lets try to start predicting from halfway down
@@ -122,5 +125,7 @@ def train_fc(learning_rate = 1e-4,
 
     ims = np.reshape(ims, (VAL_SET_SIZE, ROW_WIDTH * COL_HEIGHT * COLOR_CHANNELS))
     big_im = f.comb_ims(ims, 32, 32)
-    f.show_im_std(big_im)
+
+    if display_info:
+        f.show_im_std(big_im)
     # all should now be predicted. lets display them!
